bb4e1cb6f0725100bd2e989f30c8edf08972c106
Andrii Avramenko <andreitvmm228@gmail.com>
2025-08-04 22:00:30 +0100

    Запобігання багів та запобігання повторення швидких відповідей

--- src/js/rock-paper-scissors.js
+++ src/js/rock-paper-scissors.js
@@ -9,6 +9,13 @@ const rpsScore = {
 const resetButton = document.querySelector('#rps-reset');
 const rpsCompCounter = document.querySelector('#rps-comp-count')
 const rpsUserCounter = document.querySelector('#rps-user-count')
+const quickAnswerHolder = document.querySelector('#rps-quick-answer')
+const quickAnswers = {
+  win: [' - Чудова гра!', ' - Продовжуй в тому ж дусі!', ' - Ти цього вартий!', ' - Так тримати!'],
+  draw: [' - Сили рівні! Хто кого далі?', ' - Спробуй ще!'],
+  loss: [' - Перемога попереду!', ' - Never give up!', ' - Буває! Ще один раунд?', ' - Вперед до реваншу!']
+}
+let lastQuickAnswer = ''
 
 // if (userName == false) {
 //   document.querySelector('#rps-user-name').textContent = userName
@@ -25,12 +32,14 @@ rpsButtons.forEach(btn => {
   });
 });
 
-playButton.addEventListener('click', startGame);
+playButton.addEventListener('click', rpsStartGame);
 
-function startGame() {
+function rpsStartGame() {
   gameResult.style.color = '#000000ff';
   const chosenElement = document.querySelector('.rps-chosen');
   if (chosenElement === null) {
+    gameResult.textContent = 'Оберіть варіант вище'
+    quickAnswerHolder.textContent = ''
     gameResult.style.color = '#e88000ff';
     return;
   }
@@ -39,30 +48,68 @@ function startGame() {
   const compChoice =
     compChoices[Math.floor(Math.random() * compChoices.length)];
   playButton.textContent = compChoice
+  if (compChoice === 'scissors') {
+    playButton.textContent = 'Комп\'ютер обрав ножиці'
+  } else if (compChoice === 'rock') {
+    playButton.textContent = 'Комп\'ютер обрав камінь'
+  } else if (compChoice === 'paper') {
+    playButton.textContent = 'Комп\'ютер обрав папір'
+  }
 
   if (chosenObject === compChoice) {
+    let availableAnswers = quickAnswers.draw.filter(ans => ans !== lastQuickAnswer);
+    let newQuickAnswer = availableAnswers[Math.floor(Math.random() * availableAnswers.length)];
     gameResult.textContent = 'Нічія!';
+    quickAnswerHolder.textContent = newQuickAnswer
     gameResult.style.color = '#e88000ff';
+    quickAnswerHolder.style.color = '#e88000ff'
+    lastQuickAnswer = newQuickAnswer
   } else if (
     (chosenObject === 'rock' && compChoice === 'scissors') ||
     (chosenObject === 'paper' && compChoice === 'rock') ||
     (chosenObject === 'scissors' && compChoice === 'paper')
   ) {
-    gameResult.textContent = 'Ви перемогли!';
-    rpsScore.userScore += 1;
-    rpsUserCounter.textContent = rpsScore.userScore
-    gameResult.style.color = '#47d524ff';
+    rpsAfterGame('win')
   } else {
+    rpsAfterGame('loss')
+  }
+  chosenElement.classList.remove('rps-chosen')
+}
+
+function rpsAfterGame(result) {
+  if (result === 'win') {
+    let availableAnswers = quickAnswers.win.filter(ans => ans !== lastQuickAnswer);
+    let newQuickAnswer = availableAnswers[Math.floor(Math.random() * availableAnswers.length)];
+    gameResult.textContent = 'Ви перемогли!';
+    quickAnswerHolder.textContent = newQuickAnswer;
+    rpsScore.userScore += 1;
+    rpsUserCounter.textContent = rpsScore.userScore;
+    gameResult.style.color = '#47d524ff';
+    quickAnswerHolder.style.color = '#47d524ff';
+    lastQuickAnswer = newQuickAnswer;
+    return
+  }
+  if (result === 'loss') {
+    let availableAnswers = quickAnswers.loss.filter(ans => ans !== lastQuickAnswer);
+    let newQuickAnswer = availableAnswers[Math.floor(Math.random() * availableAnswers.length)];
     gameResult.textContent = "Комп'ютер переміг!";
+    quickAnswerHolder.textContent = newQuickAnswer
     rpsScore.compScore += 1;
     rpsCompCounter.textContent = rpsScore.compScore
     gameResult.style.color = '#f30707ff';
+    quickAnswerHolder.style.color = '#f30707ff'
+    lastQuickAnswer = newQuickAnswer
+    return
   }
+  gameResult.textContent = "Сталася помилка!";
+  quickAnswerHolder.textContent = " - Спробуйте ще раз"
+  gameResult.style.color = '#e88000ff';
+  quickAnswerHolder.style.color = '#e88000ff'
 }
 
-resetButton.addEventListener('click', resetCounter);
+resetButton.addEventListener('click', rpsResetCounter);
 
-function resetCounter() {
+function rpsResetCounter() {
   rpsScore.compScore = 0;
   rpsScore.userScore = 0;
   document.querySelector('#rps-user-count').textContent = rpsScore.userScore;

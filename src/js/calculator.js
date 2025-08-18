6a6e36a64c16d7c31f5823996393f48ff9e136cb
Katya-Semeniuk <katyasemeniuk@gmail.com>
2025-08-18 22:52:17 +0200

    fixed calculator

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,49 +1,91 @@
+const btn = document.getElementById('btn-calculate');
+const resultCalculator = document.getElementById('calculator-result');
+
+
 let operation = null;
 
-window.setOperation = function(op) {
-    operation = op;
-};
+document.querySelectorAll('.operation').forEach(button => {
+  button.addEventListener('click', () => {
+   
+    operation = button.dataset.op;
+  });
+});
 
-const waitForCalculator = setInterval(() => {
-    const btn = document.getElementById('btn');
-    const resultCalculator = document.getElementById('result');
 
-    if (btn && resultCalculator) {
-        clearInterval(waitForCalculator); 
-        btn.addEventListener('click', () => {
-            const num1 = parseFloat(document.getElementById('num1').value);
-            const num2 = parseFloat(document.getElementById('num2').value);
+btn.addEventListener('click', () => {
+    console.log("click")
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
 
-            if (isNaN(num1) || isNaN(num2)) {
-                resultCalculator.value = "Введіть обидва числа";
-                return;
-            }
-
-            if (!operation) {
-                resultCalculator.value = "Оберіть операцію";
-                return;
-            }
-
-            let result;
-            switch (operation) {
-                case '+': result = num1 + num2; break;
-                case '-': result = num1 - num2; break;
-                case '*': result = num1 * num2; break;
-                case '/':
-                    if (num2 === 0) {
-                        resultCalculator.value = "Ділення на нуль";
-                        return;
-                    }
-                    result = num1 / num2;
-                    break;
-                default:
-                    resultCalculator.value = "Невідома операція";
-                    return;
-            }
-            resultCalculator.value = result;
-        });
+    if (isNaN(num1) || isNaN(num2)) {
+        resultCalculator.value = "Введіть обидва числа";
+        return;
     }
-}, 200); 
+
+    if (!operation) {
+        resultCalculator.value = "Оберіть операцію";
+        return;
+    }
+
+    let result;
+    switch (operation) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/':
+            if (num2 === 0) {
+                resultCalculator.value = "Ділення на нуль";
+                return;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            resultCalculator.value = "Невідома операція";
+            return;
+    }
+    resultCalculator.value = result;
+})
+// const waitForCalculator = setInterval(() => {
+
+
+//     if (btn && resultCalculator) {
+//         clearInterval(waitForCalculator);
+//         btn.addEventListener('click', () => {
+//             const num1 = parseFloat(document.getElementById('num1').value);
+//             const num2 = parseFloat(document.getElementById('num2').value);
+
+//             if (isNaN(num1) || isNaN(num2)) {
+//                 resultCalculator.value = "Введіть обидва числа";
+//                 return;
+//             }
+
+//             if (!operation) {
+//                 resultCalculator.value = "Оберіть операцію";
+//                 return;
+//             }
+
+//             let result;
+//             switch (operation) {
+//                 case '+': result = num1 + num2; break;
+//                 case '-': result = num1 - num2; break;
+//                 case '*': result = num1 * num2; break;
+//                 case '/':
+//                     if (num2 === 0) {
+//                         resultCalculator.value = "Ділення на нуль";
+//                         return;
+//                     }
+//                     result = num1 / num2;
+//                     break;
+//                 default:
+//                     resultCalculator.value = "Невідома операція";
+//                     return;
+//             }
+//             resultCalculator.value = result;
+//         });
+//     }
+// }, 200); 
+
+
 
 
 
